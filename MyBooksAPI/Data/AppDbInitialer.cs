02f8c809064aa38c31126b16bb83028ca0d743c2
Chingiz Agha <cingiz167@gmail.com>
2021-10-16 18:21:26 +0400

    Update

---
+++ MyBooksAPI/Data/AppDbInitialer.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyBooksAPI.Data
+{
+    public class AppDbInitialer
+    {
+        public static void Seed(IApplicationBuilder applicationBuilder)
+        {
+
+        }
+    }
+}
